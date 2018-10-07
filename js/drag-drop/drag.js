ccd69ad7b1dde844bf39448a745b258cb2a54e5d
sumit bansal <sumit.bansal@loconav.com>
2018-10-07 21:55:45 +0530

    ul list created drag drop

--- js/drag-drop/drag.js
+++ js/drag-drop/drag.js
@@ -34,23 +34,63 @@
 //             $( "#13" ).draggable();
 
 // });
-function dragstart_handler(ev) {
-   debugger
+// function dragstart_handler(ev) {
+//    debugger
 
- console.log("dragStart");
- // Add the target element's id to the data transfer object
- ev.dataTransfer.setData("text/plain", ev.target.id);
-ev.dataTransfer.dropEffect = "move";
+//  console.log("dragStart");
+//  // Add the target element's id to the data transfer object
+//  ev.dataTransfer.setData("text/plain", ev.target.id);
+// ev.dataTransfer.dropEffect = "move";
 
-}
-function dragover_handler(ev) {
- ev.preventDefault();
- // Set the dropEffect to move
- ev.dataTransfer.dropEffect = "move"
-}
-function drop_handler(ev) {
- ev.preventDefault();
- // Get the id of the target and add the moved element to the target's DOM
- var data = ev.dataTransfer.getData("text/plain");
- ev.target.appendChild(document.getElementById(data));
-}
+// }
+// function dragover_handler(ev) {
+//  ev.preventDefault();
+//  // Set the dropEffect to move
+//  ev.dataTransfer.dropEffect = "move"
+// }
+// function drop_handler(ev) {
+//  ev.preventDefault();
+//  // Get the id of the target and add the moved element to the target's DOM
+//  var data = ev.dataTransfer.getData("text/plain");
+//  ev.target.appendChild(document.getElementById(data));
+// }
+
+function DragDrop(liList, ulList) {
+  this.$liList = $(liList);
+  this.$ulList = $(ulList);
+};
+
+DragDrop.prototype.bindEvents = function() {
+//   debugger
+// this.$liList.on('click', this.clickData());
+};
+
+DragDrop.prototype.clickData = function() {
+  var _this = this;
+  return function () {
+    console.log($(this).data());
+  };
+
+};
+
+DragDrop.prototype.init = function() {
+  // debugger{ cursor: 'move' ,containment: "body", appendTo: "body"}
+  this.$liList.draggable({
+    start: function( event, ui ) {
+      event.target.classList.add('styleAdd');
+    },
+    stop: function( event, ui ) {
+      event.target.classList.remove('styleAdd');
+    }
+  });
+  this.$ulList.droppable({
+    drop: function( event, ui ) {
+      event.target.append(ui.draggable.context);
+    }
+  });
+};
+
+$( document ).ready(function() {
+  var dragDrop = new DragDrop('.li-draggable', '.ul-droppable');
+  dragDrop.init();
+});
