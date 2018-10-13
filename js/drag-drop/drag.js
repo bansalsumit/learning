0f1044eeee4224508f727b0c150725696e61413f
sumit bansal <sumit.bansal@loconav.com>
2018-10-13 21:28:17 +0530

    drag-drop changes fixed

--- js/drag-drop/drag.js
+++ js/drag-drop/drag.js
@@ -1,80 +1,9 @@
-// function DragDrop(selectBox) {
-//   this.$selectBox = $(selectBox);
-// };
-
-// DragDrop.prototype.bindEvents = function() {
-//   this.$selectBox.on('change', this.swapOption);
-// };
-
-// DragDrop.prototype.swapOption = function() {
-//   detachedOption = $(this).find(':selected').detach();
-//   detachedOption.appendTo($(this).siblings());
-// };
-
-// $( document ).ready(function() {
-//   var dragDrop = new DragDrop('select');
-//   dragDrop.bindEvents();
-// });
-// function DragDrop(selectBox) {
-//   this.$selectBox = $(selectBox);
-// };
-
-// DragDrop.prototype.bindEvents = function() {
-//   this.$selectBox.on('change', this.swapOption);
-// };
-
-// DragDrop.prototype.swapOption = function() {
-//   detachedOption = $(this).find(':selected').detach();
-//   detachedOption.appendTo($(this).siblings());
-// };
-
-// $( document ).ready(function() {
-// //   var dragDrop = new DragDrop('select');
-// //   dragDrop.bindEvents();
-//             $( "#13" ).draggable();
-
-// });
-// function dragstart_handler(ev) {
-//    debugger
-
-//  console.log("dragStart");
-//  // Add the target element's id to the data transfer object
-//  ev.dataTransfer.setData("text/plain", ev.target.id);
-// ev.dataTransfer.dropEffect = "move";
-
-// }
-// function dragover_handler(ev) {
-//  ev.preventDefault();
-//  // Set the dropEffect to move
-//  ev.dataTransfer.dropEffect = "move"
-// }
-// function drop_handler(ev) {
-//  ev.preventDefault();
-//  // Get the id of the target and add the moved element to the target's DOM
-//  var data = ev.dataTransfer.getData("text/plain");
-//  ev.target.appendChild(document.getElementById(data));
-// }
-
-function DragDrop(liList, ulList) {
+function List(liList, ulList) {
   this.$liList = $(liList);
   this.$ulList = $(ulList);
 };
 
-DragDrop.prototype.bindEvents = function() {
-//   debugger
-// this.$liList.on('click', this.clickData());
-};
-
-DragDrop.prototype.clickData = function() {
-  var _this = this;
-  return function () {
-    console.log($(this).data());
-  };
-
-};
-
-DragDrop.prototype.init = function() {
-  // debugger{ cursor: 'move' ,containment: "body", appendTo: "body"}
+List.prototype.init = function() {
   this.$liList.draggable({
     start: function( event, ui ) {
       event.target.classList.add('styleAdd');
@@ -91,6 +20,6 @@ DragDrop.prototype.init = function() {
 };
 
 $( document ).ready(function() {
-  var dragDrop = new DragDrop('.li-draggable', '.ul-droppable');
-  dragDrop.init();
+  var country_list = new List('.li-draggable', '.ul-droppable');
+  country_list.init();
 });
